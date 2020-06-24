389daffb8498cd28bd4582bc0bb5b31504f9e11c
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-24 10:56:12 -0700

    add second behavior logic for splitting words into syllables and design a passing test for it
    Co-Authored-By: Megan Hepner <megan.hepner@gmail.com>

--- _tests_/test.js
+++ _tests_/test.js
@@ -1,8 +1,15 @@
-import { allLetter } from "./../src/syllable.js";
+import { allLetter, syllables } from "./../src/syllable.js";
 
 describe('allLetter', () => {
 test('should return false if string includes characters that are not letters or apostrophes', () => {
-  const input = ("won't");
-  expect(allLetter(input)).toEqual(true);
+  const input = ("123");
+  expect(allLetter(input)).toEqual(false);
   });
 });
+
+describe('syllables', () => {
+  test('should split each word into syllables', () => {
+    const input = ("mysterious");
+    expect(syllables(input)).toEqual(4); 
+    });
+  });
--- src/syllable.js
+++ src/syllable.js
@@ -7,3 +7,10 @@ export function allLetter(input) {
     return false;
   }
 }
+export function syllables(word) {
+  word = word.toLowerCase();
+  if(word.length <= 3) { return 1; }
+    return word.replace(/(?:[^laeiouy]es|ed|lle|[^laeiouy]e)$/, '')
+      .replace(/^y/, '')
+      .match(/[aeiouy]{1,2}/g).length;
+}
